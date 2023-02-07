4f762750091071d815236380a12c181affb92415
Tobias Chavarria <tobias@adaltas.com>
2023-02-07 14:00:40 +0100

    ci: list packages

--- .github/CI_DEPENDENCIES/core.sh
+++ .github/CI_DEPENDENCIES/core.sh
@@ -3,6 +3,3 @@
 # Install java dependencies
 sudo apt install -y openssh-server openssl && sudo systemctl start sshd
 sudo sudo apt install -y openjdk-8-jre-headless
-# SSH Key Generation
-sudo ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ''
-cat ~/.ssh/id_ed25519.pub > ~/.ssh/authorized_keys
